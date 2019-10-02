1069f7a36c07b863382e098785e8ffd74530edb0
AarushiKapoor <54901924+AarushiKapoor@users.noreply.github.com>
2019-10-03 02:48:58 +0530

    Update Binary Search.cpp

--- Search/Binary Search.cpp
+++ Search/Binary Search.cpp
@@ -1,36 +1,49 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int binary_search(int a[], int l, int r, int key)
+class Searching
 {
-	while (l <= r)
-	{
-		int m = l + (r - l) / 2;
-		if (key == a[m])
-			return m;
-		else if (key < a[m])
-			r = m - 1;
-		else
-			l = m + 1;
-	}
-	return -1;
+    int a[10],n,i;
+public:
+    void Input();
+    void Binary();
+};
+void Searching::Input()
+{
+    cout<<"Enter Total number of elements:";
+    cin>>n;
+    cout<<"\nEnter them one by one:";
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
 }
-int main(int argc, char const *argv[])
+//Binary Search
+void Searching::Binary()
 {
-	int n, key;
-	cout << "Enter size of array: ";
-	cin >> n;
-	cout << "Enter array elements: ";
-	int a[n];
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> a[i];
-	}
-	cout << "Enter search key: ";
-	cin >> key;
-	int res = binary_search(a, 0, n - 1, key);
-	if (res != -1)
-		cout << key << " found at index " << res << endl;
-	else
-		cout << key << " not found" << endl;
-	return 0;
+    int ele,f=0,F,M,L;
+    cout<<"\nEnter any element to be searched:";
+    cin>>ele;
+    F=0;
+    L=n-1;
+    while((F<=L)&&(f==0))
+    {
+        M=(F+L)/2;
+        if(a[M]==ele)
+            f=1;
+        else if(a[M]<ele)
+            F=M+1;
+        else
+            L=M-1;
+    }
+    if(f==0)
+        cout<<"Element does not exist";
+    else
+        cout<<"\nElement exists in "<<M<<" position";
+}
+int main()
+{
+    Searching S;
+    S.Input();
+    S.Binary();
+    return 0;
 }
